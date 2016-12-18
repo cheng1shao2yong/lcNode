33e8f26e46475461477da732ee06ecf12496c314
老成有木有 <85556713@qq.com>
2016-12-18 14:26:11 +0800

    no commit message

--- compile/foot.js
+++ compile/foot.js
@@ -1,6 +1,6 @@
 function temp(resp,val){
 with(val){
-resp.write("<p>"+test+"这里head"+abc.tf.a+"</p>\n");
+resp.write("<p>"+test+"这里foot"+abc.tf.a+"</p>\n");
 }
 }
 module.exports=temp;
--- compile/index.js
+++ compile/index.js
@@ -7,6 +7,12 @@ resp.write("    <meta charset=\"UTF-8\">\n");
 resp.write("    <title>测试模版</title>\n");
 resp.write("</head>\n");
 resp.write("<body>\n");
+        (function(){
+        var a=6;
+        var b=7;
+        resp.write(a+b+"<br/>");
+        resp.write(test+"<br/>");
+        })();
 resp.write("    ");require("./head")(resp,val);resp.write("\n");
 resp.write("    <p>"+test+"这里是变量调用22222222"+abc.tf.a+"</p>\n");
 resp.write("    <p>这里是if条件判断");if(test==abc.tf.a){resp.write("123");}resp.write("</p>\n");
@@ -27,7 +33,13 @@ resp.write("        ");if(v1%2==0){resp.write("\n");
 resp.write("            "+k1+"=>"+v1+"<br/>\n");
 resp.write("        ");}resp.write("\n");
 resp.write("    ");}resp.write("\n");
-resp.write("    ");require("./head")(resp,val);resp.write("\n");
+resp.write("    ");require("./foot")(resp,val);resp.write("\n");
+        (function(){
+        var a=1;
+        var b=2;
+        resp.write(a+b+"<br/>");
+        resp.write(test+"<br/>");
+        })();
 resp.write("</body>\n");
 resp.write("</html>\n");
 }
